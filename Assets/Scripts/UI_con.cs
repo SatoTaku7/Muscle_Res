85ee5d40c2ce89112759b14f28bcb1d50023abf3
SatoTaku7 <98199671+SatoTaku7@users.noreply.github.com>
2023-09-21 17:37:12 +0900

    textの表記など細かい調整

--- Assets/Scripts/UI_con.cs
+++ Assets/Scripts/UI_con.cs
@@ -4,16 +4,15 @@ using UnityEngine;
 
 public class UI_con : MonoBehaviour
 {
-    [SerializeField] GameObject Player;
-    OVRInput.Controller L_con;
+    OVRInput.Controller L_con = OVRInput.Controller.LTouch;
     void Start()
     {
-        L_con = OVRInput.Controller.LTouch;
+        
     }
 
     void Update()
     {
         Vector3 L_accel = OVRInput.GetLocalControllerAcceleration(L_con);
-        Debug.Log("L_accel:" + L_accel );
+        Debug.Log( L_accel );
     }
 }
